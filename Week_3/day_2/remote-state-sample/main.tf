19c1a1f97b6fba2b65ed4f23c5f40e2ed867eb83
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-03 12:00:19 +0100

    screenshot added for labs

--- Week_3/day_2/remote-state-sample/main.tf
+++ Week_3/day_2/remote-state-sample/main.tf
@@ -1,17 +1,17 @@
 variable "ami_image_name" {
   description = "this is a variable for the AMI image"
-  type = string
-  default = "value"
+  type        = string
+  default     = "ami-0c76bd4bd302b30ec"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
-  type = string
+  default     = "t2.micro"
+  type        = string
   description = "this is the variable for the instance type"
 }
 
 resource "aws_instance" "jena_web_server" {
-  ami = var.ami_image_name
+  ami           = var.ami_image_name
   instance_type = var.instance_type
 
   tags = {
--- Week_3/day_2/remote-state-sample/providers.tf
+++ Week_3/day_2/remote-state-sample/providers.tf
@@ -1,8 +1,14 @@
 terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+      
+      }
+    }
   backend "s3" {
-    bucket = "jenagansivakumar-s3-bucket"
-    key = "terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "jenagan-sivakumar-s3-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "statelock"
   }
 }

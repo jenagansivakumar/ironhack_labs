a3cc27a3e4648f215988423961a879e39f1c99d6
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-04 15:02:56 +0100

    finished second task

--- Week_3/day_3/count_meta_argument/count-example/ec2_instance.tf
+++ Week_3/day_3/count_meta_argument/count-example/ec2_instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "jena_ec2_instance" {
-  count         = 4
+  count         = var.ec2_count
   ami           = "ami-0c76bd4bd302b30ec"
   instance_type = var.instance_type
   tags = {
-    Name = "Instance ${count.index + 1}"
+    Name = var.ec2_names[count.index]
   }
 }
--- Week_3/day_3/count_meta_argument/count-example/variables.tf
+++ Week_3/day_3/count_meta_argument/count-example/variables.tf
@@ -3,3 +3,13 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "ec2_count" {
+  type    = number
+  default = 4
+}
+
+variable "ec2_names" {
+  type    = list(string)
+  default = ["jena-instance-1", "jena-instance-2", "jena-instance-3", "jena-instance-4"]
+}

be32017ba6bb0bb399210c20527a0954e04942fd
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-04 14:41:44 +0100

    created ec2 instance with count

---
+++ Week_3/day_3/count_meta_argument/count-example/ec2_instance.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "jena_ec2_instance" {
+  count = 4
+  ami = "ami-0c76bd4bd302b30ec"
+  instance_type = var.instance_type
+  tags = {
+    Name = "Instance ${count.index}"
+  }
+}
---
+++ Week_3/day_3/count_meta_argument/count-example/variables.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {
+  description = "This is the type of the instance"
+  type = string
+  default = "t2.micro"
+}
